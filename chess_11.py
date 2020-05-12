9c3fbade6058393238bbb693eea85129d3d540c9
Tac1701 <65177943+Tac1701@users.noreply.github.com>
2020-05-12 11:54:53 +0200

    Voici (normalement) la liste

---
+++ chess_11.py
@@ -0,0 +1,9 @@
+#  chess_11.py
+#  
+
+
+class Plateau:
+	def __init__(self):
+		
+		self.coords=[list(range(10,18)),list(range(20,28)),list(range(30,38)),list(range(40,48)),
+		list(range(50,58)),list(range(60,68)),list(range(70,78)),list(range(80,88))]

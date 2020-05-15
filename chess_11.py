0233360213c9790afa9eded238d2f6c851045b51
Tac1701 <taibac2002@gmail.com>
2020-05-15 12:34:49 +0200

    Update chess_11.py

--- chess_11.py
+++ chess_11.py
@@ -5,11 +5,11 @@ class Plateau:
 
     def __init__(self):
         self.player_turn = "White"
-        self.plateau_coor = [list(range(10, 18)), list(range(20, 28)), list(range(30, 38)), list(range(40, 48)),
-                        list(range(50, 58)), list(range(60, 68)), list(range(70, 78)), list(range(80, 88))]
+        self.plateau_coor = [list(range(11, 19)), list(range(21, 29)), list(range(31, 39)), list(range(41, 49)),
+                        list(range(51, 59)), list(range(61, 69)), list(range(71, 79)), list(range(81, 89))]
 
         self.plateau_init = [
-                        ["TN1", "CN1", "FN1", "EN1", "RN1", "FN2", "CN2", "TR2"],
+                        ["TN1", "CN1", "FN1", "EN1", "RN1", "FN2", "CN2", "TN2"],
                         ["PN1", "PN2", "PN3", "PN4", "PN5", "PN6", "PN7", "PN8"],
                         ["", "", "", "", "", "", "", ""],
                         ["", "", "", "", "", "", "", ""],
@@ -30,10 +30,21 @@ class Plateau:
             coord = self.plateau_coor[b][a]
             return [selected_piece, coord]
 
-    def two_case_pion_mvt(self,selected_piece):
-        coord=find_coord_of_piece(self, selected_piece)
-        
-        
+    def two_case_pion_mvt(self,selected_piece): #test
+        coord=self.find_coord_of_piece( selected_piece)
+        two=False
+        color=selected_piece[1]
+        if color=="N":
+            for i in range (21,29):
+                if i==coord:
+                    two=True
+        if color=="B":
+            for i in range (71,79):
+                if i==coord:
+                    two=True
+        if two:
+            a=piece_11.Pion(selected_piece)#test
+            a.mvt=[1,2]#test
 
     def check_piece(self, go_to_position):  #vérifie si une piece se trouve dans la case de destination
         n = str(go_to_position)
@@ -100,3 +111,4 @@ class Plateau:
 plateau = Plateau()
 print(plateau.find_coord_of_piece(""))
 plateau.display()
+print(plateau.two_case_pion_mvt("TB1"))
